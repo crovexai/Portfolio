0cde5cecccefa001b9f4857a4785aa3b7748671d
crovexai <crovexai@gmail.com>
2026-04-19 14:44:21 -0400

    Add Layout component with Navbar

---
+++ components/Layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+export default function Layout({ children }: any) {
+  return (
+    <div className="container">
+      <Navbar />
+      {children}
+    </div>
+  );
+}
