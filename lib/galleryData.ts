968e9d143130dcae63aa9829df207aa6d4ee0ee0
crovexai <crovexai@gmail.com>
2026-04-19 14:43:20 -0400

    Add gallery data for home, fashion, fitness, and lifestyle

---
+++ lib/galleryData.ts
@@ -0,0 +1,35 @@
+export const homeImages = [
+  "/images/home/1.jpg",
+  "/images/home/2.jpg",
+  "/images/home/3.jpg",
+  "/images/home/4.jpg",
+  "/images/home/5.jpg",
+  "/images/home/6.jpg"
+];
+
+export const fashionImages = [
+  "/images/fashion/1.jpg",
+  "/images/fashion/2.jpg",
+  "/images/fashion/3.jpg",
+  "/images/fashion/4.jpg",
+  "/images/fashion/5.jpg",
+  "/images/fashion/6.jpg"
+];
+
+export const fitnessImages = [
+  "/images/fitness/1.jpg",
+  "/images/fitness/2.jpg",
+  "/images/fitness/3.jpg",
+  "/images/fitness/4.jpg",
+  "/images/fitness/5.jpg",
+  "/images/fitness/6.jpg"
+];
+
+export const lifestyleImages = [
+  "/images/lifestyle/1.jpg",
+  "/images/lifestyle/2.jpg",
+  "/images/lifestyle/3.jpg",
+  "/images/lifestyle/4.jpg",
+  "/images/lifestyle/5.jpg",
+  "/images/lifestyle/6.jpg"
+];
