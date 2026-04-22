6baa8185d599bcdff43b15f0b59e0403f359c2a5
crovexai <crovexai@gmail.com>
2026-04-22 04:25:40 -0400

    Add new image URLs to homeImages array
    Updated homeImages array with new image URLs.

--- data.js
+++ data.js
@@ -1,10 +1,10 @@
 const homeImages = [
-  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-1.jpg",
-  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-2.jpg",
-  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-3.png",
-  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-4.jpg",
-  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-5.jpg",
-  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-6.jpg"
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-1_cxlash",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-2_kfaujz",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-3_wd6oaa",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-4_wqqgw7",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-5_mud39s",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-6_rhleyv"
 ];
 
 const fashionImages = [
