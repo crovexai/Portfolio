b4080dee0f19c50c506da4b248392d19da13e9df
crovexai <crovexai@gmail.com>
2026-04-22 06:45:24 +0000

    Convert Next.js portfolio to pure HTML/CSS/JS for GitHub Pages

--- components/GalleryGrid.tsx
+++
@@ -1,9 +0,0 @@
-export default function GalleryGrid({ images }: any) {
-  return (
-    <div className="grid">
-      {images.map((src: string, i: number) => (
-        <img key={i} src={src} className="img" />
-      ))}
-    </div>
-  );
-}
