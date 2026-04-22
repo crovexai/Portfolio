e09e33373dc561c7731af1ea2b8beaf081f62e0a
crovexai <crovexai@gmail.com>
2026-04-22 08:28:22 -0400

    Initialize fashion and lifestyle images arrays

--- data.js
+++ data.js
@@ -7,13 +7,32 @@ const homeImages = [
   "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-6_rhleyv.webp"
 ];
 
-const fashionImages = [];
-
-const commercialImages = [
-  // ADD YOUR COMMERCIAL IMAGES HERE LATER
+const fashionImages = [
+ "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-4_wqqgw7.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-2_kfaujz.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-3_wd6oaa.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-1_cxlash.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-5_mud39s.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-6_rhleyv.webp"
 ];
 
-const lifestyleImages = [];
+const commercialImages = [
+ "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-4_wqqgw7.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-2_kfaujz.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-3_wd6oaa.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-1_cxlash.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-5_mud39s.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-6_rhleyv.webp"
+];
+
+const lifestyleImages = [
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-4_wqqgw7.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-2_kfaujz.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-3_wd6oaa.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-1_cxlash.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-5_mud39s.webp",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/v1776844074/img-home-6_rhleyv.webp"
+];
 
 function renderGallery(images) {
   const grid = document.getElementById("gallery");
