367c8030316b8f841618a4a22ad67a0f627f6459
crovexai <crovexai@gmail.com>
2026-04-22 02:35:40 -0400

    Update image paths in galleryData.ts

--- lib/galleryData.ts
+++ lib/galleryData.ts
@@ -8,28 +8,28 @@ export const homeImages = [
 ];
 
 export const fashionImages = [
-  "/images/fashion/1.jpg",
-  "/images/fashion/2.jpg",
-  "/images/fashion/3.jpg",
-  "/images/fashion/4.jpg",
-  "/images/fashion/5.jpg",
-  "/images/fashion/6.jpg"
+  "/images/fashion/img-fashion-1.jpg",
+  "/images/fashion/img-fashion-2.jpg",
+  "/images/fashion/img-fashion-3.jpg",
+  "/images/fashion/img-fashion-4.jpg",
+  "/images/fashion/img-fashion-5.jpg",
+  "/images/fashion/img-fashion-6.jpg"
 ];
 
 export const fitnessImages = [
-  "/images/fitness/1.jpg",
-  "/images/fitness/2.jpg",
-  "/images/fitness/3.jpg",
-  "/images/fitness/4.jpg",
-  "/images/fitness/5.jpg",
-  "/images/fitness/6.jpg"
+  "/images/fitness/img-fitness-1.jpg",
+  "/images/fitness/img-fitness-2.jpg",
+  "/images/fitness/img-fitness-3.jpg",
+  "/images/fitness/img-fitness-4.jpg",
+  "/images/fitness/img-fitness-5.jpg",
+  "/images/fitness/img-fitness-6.jpg"
 ];
 
 export const lifestyleImages = [
-  "/images/lifestyle/1.jpg",
-  "/images/lifestyle/2.jpg",
-  "/images/lifestyle/3.jpg",
-  "/images/lifestyle/4.jpg",
-  "/images/lifestyle/5.jpg",
-  "/images/lifestyle/6.jpg"
+  "/images/lifestyle/img-lifestyle-1.jpg",
+  "/images/lifestyle/img-lifestyle-2.jpg",
+  "/images/lifestyle/img-lifestyle-3.jpg",
+  "/images/lifestyle/img-lifestyle-4.jpg",
+  "/images/lifestyle/img-lifestyle-5.jpg",
+  "/images/lifestyle/img-lifestyle-6.jpg"
 ];
