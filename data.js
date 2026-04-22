d6a76a2dd88e4b5af0543956187abc56f201d2e4
crovexai <crovexai@gmail.com>
2026-04-22 06:59:22 +0000

    Fix broken gallery images - use home images for all galleries temporarily

--- data.js
+++ data.js
@@ -8,30 +8,30 @@ const homeImages = [
 ];
 
 const fashionImages = [
-  "/images/fashion/img-fashion-1.jpg",
-  "/images/fashion/img-fashion-2.jpg",
-  "/images/fashion/img-fashion-3.jpg",
-  "/images/fashion/img-fashion-4.jpg",
-  "/images/fashion/img-fashion-5.jpg",
-  "/images/fashion/img-fashion-6.jpg"
+  "/images/home/img-home-1.jpg",
+  "/images/home/img-home-2.jpg",
+  "/images/home/img-home-3.png",
+  "/images/home/img-home-4.jpg",
+  "/images/home/img-home-5.jpg",
+  "/images/home/img-home-6.jpg"
 ];
 
 const fitnessImages = [
-  "/images/fitness/img-fitness-1.jpg",
-  "/images/fitness/img-fitness-2.jpg",
-  "/images/fitness/img-fitness-3.jpg",
-  "/images/fitness/img-fitness-4.jpg",
-  "/images/fitness/img-fitness-5.jpg",
-  "/images/fitness/img-fitness-6.jpg"
+  "/images/home/img-home-1.jpg",
+  "/images/home/img-home-2.jpg",
+  "/images/home/img-home-3.png",
+  "/images/home/img-home-4.jpg",
+  "/images/home/img-home-5.jpg",
+  "/images/home/img-home-6.jpg"
 ];
 
 const lifestyleImages = [
-  "/images/lifestyle/img-lifestyle-1.jpg",
-  "/images/lifestyle/img-lifestyle-2.jpg",
-  "/images/lifestyle/img-lifestyle-3.jpg",
-  "/images/lifestyle/img-lifestyle-4.jpg",
-  "/images/lifestyle/img-lifestyle-5.jpg",
-  "/images/lifestyle/img-lifestyle-6.jpg"
+  "/images/home/img-home-1.jpg",
+  "/images/home/img-home-2.jpg",
+  "/images/home/img-home-3.png",
+  "/images/home/img-home-4.jpg",
+  "/images/home/img-home-5.jpg",
+  "/images/home/img-home-6.jpg"
 ];
 
 function renderGallery(images) {
