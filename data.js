e2a7be9c0ef98e34e5f4c639e06872f231ce3d61
crovexai <crovexai@gmail.com>
2026-04-22 08:19:18 +0000

    Update image URLs to use Cloudinary

--- data.js
+++ data.js
@@ -1,37 +1,37 @@
 const homeImages = [
-  "/images/home/img-home-1.jpg",
-  "/images/home/img-home-2.jpg",
-  "/images/home/img-home-3.png",
-  "/images/home/img-home-4.jpg",
-  "/images/home/img-home-5.jpg",
-  "/images/home/img-home-6.jpg"
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-1.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-2.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-3.png",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-4.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-5.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/home/img-home-6.jpg"
 ];
 
 const fashionImages = [
-  "/images/home/img-home-1.jpg",
-  "/images/home/img-home-2.jpg",
-  "/images/home/img-home-3.png",
-  "/images/home/img-home-4.jpg",
-  "/images/home/img-home-5.jpg",
-  "/images/home/img-home-6.jpg"
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fashion/img-fashion-1.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fashion/img-fashion-2.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fashion/img-fashion-3.png",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fashion/img-fashion-4.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fashion/img-fashion-5.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fashion/img-fashion-6.jpg"
 ];
 
 const fitnessImages = [
-  "/images/home/img-home-1.jpg",
-  "/images/home/img-home-2.jpg",
-  "/images/home/img-home-3.png",
-  "/images/home/img-home-4.jpg",
-  "/images/home/img-home-5.jpg",
-  "/images/home/img-home-6.jpg"
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fitness/img-fitness-1.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fitness/img-fitness-2.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fitness/img-fitness-3.png",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fitness/img-fitness-4.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fitness/img-fitness-5.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/fitness/img-fitness-6.jpg"
 ];
 
 const lifestyleImages = [
-  "/images/home/img-home-1.jpg",
-  "/images/home/img-home-2.jpg",
-  "/images/home/img-home-3.png",
-  "/images/home/img-home-4.jpg",
-  "/images/home/img-home-5.jpg",
-  "/images/home/img-home-6.jpg"
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/lifestyle/img-lifestyle-1.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/lifestyle/img-lifestyle-2.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/lifestyle/img-lifestyle-3.png",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/lifestyle/img-lifestyle-4.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/lifestyle/img-lifestyle-5.jpg",
+  "https://res.cloudinary.com/dovrzmlqj/image/upload/f_auto,q_auto,w_1600/lifestyle/img-lifestyle-6.jpg"
 ];
 
 function renderGallery(images) {
